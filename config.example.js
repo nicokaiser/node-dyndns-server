0f4f13bbc9faf40cea66489a64273ba511159e77
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-02-18 16:12:39 +0100

    Example config

---
+++ config.example.js
@@ -0,0 +1,22 @@
+var config = module.exports = {};
+
+config.port = process.env.PORT || 3000;
+config.host = 'localhost';
+
+config.users = {
+    "user1": {
+        "password": "...",
+        "hosts": ["mydomain1", "mydomain2"]
+    }
+};
+
+config.bind = {
+    server: 'localhost',
+    zone: 'dyn.example.com',
+    ttl: 60,
+    key: {
+        name: 'ddns-key',
+        algorithm: 'hmac-sha256',
+        secret: '...'
+    }
+};
